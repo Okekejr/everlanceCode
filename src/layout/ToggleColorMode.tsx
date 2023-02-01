be03a3c36ba4aa27fb56de7b0704c07d5a8a7920
Emmanuel <85338878+Okekejr@users.noreply.github.com>
2023-01-31 18:21:56 -0600

    navbar, logo, darkMode

---
+++ src/layout/ToggleColorMode.tsx
@@ -0,0 +1,20 @@
+import { Button, useColorMode } from "@chakra-ui/react";
+import { HiOutlineSun, HiOutlineMoon } from "react-icons/hi2";
+import { FC } from "react";
+
+export const ToggleColorMode: FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Button
+      variant="none"
+      onClick={() => toggleColorMode()}
+      pos="absolute"
+      top={{ base: "-1", md: "-2" }}
+      right="0"
+      m="1rem"
+    >
+      {colorMode === "dark" ? <HiOutlineSun /> : <HiOutlineMoon />}
+    </Button>
+  );
+};
